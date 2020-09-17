f527fff0353952691da007fbceb98601f778858f
aga97 <aga97@naver.com>
2020-09-17 19:01:02 +0900

    authentication

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import AppRouter from './Router';
+import React, { useEffect, useState } from 'react';
+import AppRouter from 'components/Router';
+import { authService } from 'fbase';
 
 //import logo from './logo.svg';
 //import './App.css';
 
 function App() {
-  return <AppRouter />;
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if(user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, [])
+  return (
+    <>
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initailizing...."}
+      <footer>&copy; {new Date().getFullYear()} cltw</footer>
+    </>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import './index.css';
 import App from './components/App';
-import firebase from "./firebase";
+import firebase from "fbase";
 //import * as serviceWorker from './serviceWorker';
 
-console.log(firebase);
+
 
 ReactDOM.render(
   <React.StrictMode>
